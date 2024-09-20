2659a13315f47da79ade2efe936615ab8ff1ce9c
Sidhant-gupta07 <sidhantguptabdk123@gmail.com>
2024-09-20 12:03:53 +0530

    update setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-300">
+      Hello world!
+    </h1>
+        
+    </>
+  )
+}
+
+export default App
